c39960dd0e9ed4892c97c18f260fe636afadc6ee
Pratham <bhagatpratham101@gmail.com>
2022-08-03 22:57:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const Data = () => {
+    localStorage.setItem("Name", JSON.stringify("Pratham"));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={Data}>Save Data</button>
     </div>
   );
 }
